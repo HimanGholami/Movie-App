0269f5322926ea6e6aa4d04b0f253002153a9b07
Himangh444 <himangholami1@gmail.com>
2026-05-07 01:24:48 +0330

    project init

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <header>
+      <div>
+        <div>
+          <div>
+            <img src="images/js.png" alt="logo" />
+          </div>
+        </div>
+        <div></div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
